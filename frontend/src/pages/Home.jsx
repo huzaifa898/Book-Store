d33ffdb8b3b687d505fbe481c7168d0c14d6dd83
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-01-18 11:17:01 +0500

    finalize the home page and setup for react doms

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import  "./Home.css"
 const Home = () => {
   return (
-    <div className="Home-Page bg-dark text-white container-fluid justify-content-center align-items-center">
-    <div className="row container">
+    <div className="Home-Page bg-dark text-white container-fluid d-flex justify-content-center align-items-center">
+    <div className="row container ">
     <div className="col-lg-6 d-flex justify-content-center align-items-start flex-column"
          style={{ height: "91.5vh" }}
          > 
-          <h2 style={{ fontSize: "70px" }}>BOOK STORE FOR YOU</h2>
+          <h2 style={{ fontSize: "80px" }}>BOOK STORE </h2>
+          <h2  style={{ fontSize: "50px" }}>FOR YOU</h2>
+          <p className='mb-0' style={{ color: "silver" }} >CheckOut The Books From Here</p>
           <button className='VeiwBook'>View Book</button>
     </div>
     <div className="col-lg-6 d-flex justify-content-center align-items-end flex-column"
